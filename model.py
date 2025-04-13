2b31a033c24798a054b8f0d5932f6e7d83f9cee7
Esteb <140031716+Timewolff@users.noreply.github.com>
2025-04-13 17:00:09 -0600

    Readme, folder notebooks creados, revisar checklist de README

---
+++ model.py
@@ -0,0 +1,11 @@
+# URL dataset: https://www.kaggle.com/datasets/ethancratchley/email-phishing-dataset?resource=download
+
+# --------------------------------------------
+# Libraries
+# --------------------------------------------
+import pandas as pd
+
+# --------------------------------------------
+# READ THE DATASET
+# --------------------------------------------
+df = pd.read_csv("data/email_phishing_data.csv")
